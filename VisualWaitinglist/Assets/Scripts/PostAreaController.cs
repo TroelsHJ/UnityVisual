aab1c00f721d22e63235ab238a28543bb6ac9bf3
Troels Højmark Jensen <troe3894@student.eadania.dk>
2018-03-16 21:23:59 +0100

    put a image on the plane, need to adjust the light. Tried messing around with the color gradient

--- VisualWaitinglist/Assets/Scripts/PostAreaController.cs
+++ VisualWaitinglist/Assets/Scripts/PostAreaController.cs
@@ -17,6 +17,28 @@ public class PostAreaController : MonoBehaviour
         amountWaiting = int.Parse(this.transform.name);
 
         SetInitScale(amountWaiting);
+        SetColorGradient();
+    }
+
+    private void SetColorGradient()
+    {
+        Gradient g = new Gradient();
+        GradientColorKey[] gck = new GradientColorKey[2];
+        GradientAlphaKey[] gak = new GradientAlphaKey[2];
+
+        gck[0].color = Color.red;
+        gck[0].time = 0.0F;
+        gck[1].color = Color.blue;
+        gck[1].time = 1.0F;
+
+        gak[0].alpha = 1.0F;
+        gak[0].time = 0.0F;
+        gak[1].alpha = 0.0F;
+        gak[1].time = 1.0F;
+
+        g.SetKeys(gck, gak);
+        this.gameObject.GetComponent<Renderer>().material.color = g.Evaluate(1);
+        Debug.Log(g.Evaluate(0.25F));
     }
 
     private void SetInitScale(int amountWaiting)
