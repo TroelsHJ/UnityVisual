eec3adbba1c4d678277005409d0297812ce961fe
Troels Højmark Jensen <troe3894@student.eadania.dk>
2018-03-16 13:54:53 +0100

    connection from gameobjects to the db is working, so now i can hover over the gameobjects and get the total size of the waiting list for the area code

--- VisualWaitinglist/Assets/Scripts/DatabaseHandler.cs
+++ VisualWaitinglist/Assets/Scripts/DatabaseHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MySql.Data;
 using MySql.Data.MySqlClient;
 using System.Data;
 using System;
@@ -34,15 +33,29 @@ public class DatabaseHandler : MonoBehaviour
         Debug.Log(conn.State);
     }
 
-    public int GetSizeOfList(int _postalNumber)
+    public int GetListForArea(int _postalNumber)
+    {
+        List<WaitList> _tempList = new List<WaitList>();
+        int totalListLength = 0;
+
+        _tempList = GetListFromDB(_postalNumber);
+
+        _tempList = FilterListEntries(_tempList);
+
+        foreach (WaitList item in _tempList)
+        {
+            totalListLength += item.ListLength;
+        }
+
+        return totalListLength;
+    }
+
+    private List<WaitList> GetListFromDB(int _postalNumber)
     {
         List<WaitList> _tempList = new List<WaitList>();
         WaitList _tempListObj;
-        int totalListLength = 0;
 
-
-        // Husk at tilføje afdeling til query!!
-        cmd.CommandText = string.Format("SELECT ventelister.AntalPaaVenteliste, afdelinger.AfdPostBy FROM ventelister INNER JOIN afdelinger ON ventelister.FK_AfdId = afdelinger.AfdId WHERE AfdPostBy = {0};", _postalNumber);
+        cmd.CommandText = string.Format("SELECT ventelister.AntalPaaVenteliste, afdelinger.AfdPostBy, afdelinger.AfdId FROM ventelister INNER JOIN afdelinger ON ventelister.FK_AfdId = afdelinger.AfdId WHERE AfdPostBy = {0};", _postalNumber);
         cmd.CommandType = CommandType.Text;
         cmd.Connection = conn;
 
@@ -54,48 +67,78 @@ public class DatabaseHandler : MonoBehaviour
         }
         reader.Close();
 
-        _tempList = FilterListEntries(_tempList);
-        foreach (WaitList item in _tempList)
-        {
-            totalListLength += item.ListLength;
-        }
-
-        return totalListLength;
+        return _tempList;
     }
 
     private List<WaitList> FilterListEntries(List<WaitList> _tempList)
     {
         WaitList[] _tempAry = _tempList.ToArray();
+        Dictionary<int, int> _tempListDictonary = new Dictionary<int, int>();
+        Dictionary<int, int> _tempCountDictonary = new Dictionary<int, int>();
 
-        for (int i = 0; i < _tempAry.Length; i++)
+        List<WaitList> _finalList = new List<WaitList>();
+
+        _tempCountDictonary = CountDepartmentOccu(_tempAry);
+
+        _tempListDictonary = AddListsPrDeparment(_tempAry);
+
+        _tempListDictonary = DivListPrDepartmentOccu(_tempListDictonary, _tempCountDictonary);
+
+        foreach (KeyValuePair<int, int> item in _tempListDictonary)
         {
-            if (_tempAry[i].Department == _tempAry[i + 1].Department)
-            {
+            _finalList.Add(new WaitList(_tempAry[0].PostalCode, item.Key, item.Value));
+        }
 
+        return _finalList;
+    }
+
+    private Dictionary<int, int> CountDepartmentOccu(WaitList[] _tempAry)
+    {
+        Dictionary<int, int> _tempCountDictonary = new Dictionary<int, int>();
+
+        for (int j = 0; j < _tempAry.Length; j++)
+        {
+            if (_tempCountDictonary.ContainsKey(_tempAry[j].Department))
+            {
+                _tempCountDictonary[_tempAry[j].Department] += 1;
+            }
+            else
+            {
+                _tempCountDictonary.Add(_tempAry[j].Department, 1);
             }
         }
 
-        return null;
+        return _tempCountDictonary;
+
     }
 
-    //public List<Bil> GetSomethingOnList()
-    //{
-    //    List<Bil> _tempCarList = new List<Bil>();
-    //    Bil _tempCar = new Bil();
-    //    cmd.CommandText = string.Format("SELECT * FROM cars WHERE ManufacturedYear = '{0}'", _year);
-    //    cmd.CommandType = CommandType.Text;
-    //    cmd.Connection = conn;
+    private Dictionary<int, int> AddListsPrDeparment(WaitList[] _tempAry)
+    {
+        Dictionary<int, int> _tempListDictonary = new Dictionary<int, int>();
 
-    //    reader = cmd.ExecuteReader();
-    //    while (reader.Read())
-    //    {
-    //        _tempCar.ID = (int)reader[0];
-    //        _tempCar.Manufacture = (string)reader[1];
-    //        _tempCar.ManufacturedYear = (int)reader[2];
-    //        _tempCarList.Add(_tempCar);
-    //    }
+        for (int i = 0; i < _tempAry.Length; i++)
+        {
+            if (_tempListDictonary.ContainsKey(_tempAry[i].Department))
+            {
+                _tempListDictonary[_tempAry[i].Department] += _tempAry[i].ListLength;
+            }
+            else
+            {
+                _tempListDictonary.Add(_tempAry[i].Department, _tempAry[i].ListLength);
+            }
+        }
 
-    //    reader.Close();
-    //    return _tempCarList;
-    //}
+        return _tempListDictonary;
+    }
+
+    private Dictionary<int, int> DivListPrDepartmentOccu(Dictionary<int, int> _tempListDictonary, Dictionary<int, int> _tempCountDictonary)
+    {
+        foreach (KeyValuePair<int, int> item in _tempCountDictonary)
+        {
+            _tempListDictonary[item.Key] /= _tempCountDictonary[item.Key];
+        }
+
+        return _tempListDictonary;
+    }
+
 }
---
+++ VisualWaitinglist/Assets/Scripts/PostAreaController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostAreaController : MonoBehaviour
+{
+    DatabaseHandler DB;
+    private int amountWaiting;
+    public ShowData showData;
+
+    void Start()
+    {
+        DB = GameObject.Find("DatabaseHandler").GetComponent<DatabaseHandler>();
+    }
+
+    private void OnMouseOver()
+    {
+        amountWaiting = DB.GetListForArea(int.Parse(this.transform.name));
+        Debug.Log(amountWaiting);
+        showData.DisplayAmountWaitingInInfoBox(amountWaiting);
+    }
+
+    private void OnMouseExit()
+    {
+        showData.DisplayAmountWaitingInInfoBox(0);
+    }
+
+}
