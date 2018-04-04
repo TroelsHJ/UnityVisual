2fe89522537e9f741e401c1e9ae8ca2d1c7c77ad
Troels Højmark Jensen <troe3894@student.eadania.dk>
2018-04-04 11:42:46 +0200

    Camera control is now working. Hold down space, and use the mouse to move the camera

---
+++ VisualWaitinglist/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    public float MovementSpeed = 25f;
+    public CinemachineFreeLook freeLookCamera;
+
+
+    private void FixedUpdate()
+    {
+        float movementY = Input.GetAxis("Vertical");
+        transform.Translate(0, movementY * Time.fixedDeltaTime * MovementSpeed, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            freeLookCamera.m_XAxis.m_InputAxisName = "Mouse X";
+            freeLookCamera.m_YAxis.m_InputAxisName = "Mouse Y";
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            freeLookCamera.m_XAxis.m_InputAxisName = "";
+            freeLookCamera.m_YAxis.m_InputAxisName = "";
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            freeLookCamera.m_Lens.FieldOfView++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            freeLookCamera.m_Lens.FieldOfView--;
+        }
+
+    }
+}
